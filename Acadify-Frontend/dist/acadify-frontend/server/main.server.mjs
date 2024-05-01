a6622dc9b0dda0ffd950d4367c9b3a663366c2d3
harshitshukla-dev <hershitshukla35@gmail.com>
2024-05-01 10:09:41 +0530

    Update import paths for frontend files

--- Acadify-Frontend/dist/acadify-frontend/server/main.server.mjs
+++ Acadify-Frontend/dist/acadify-frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VFQY7OGB.mjs";import"./chunk-RH7I6IWJ.mjs";import"./chunk-QYG7H2UI.mjs";export{a as default};
+import{a}from"./chunk-4TWPNJLC.mjs";import"./chunk-RH7I6IWJ.mjs";import"./chunk-QYG7H2UI.mjs";export{a as default};
